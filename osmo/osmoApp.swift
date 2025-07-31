066aebf7d12dca4d1d88902707c3dc6687a9cb7b
Mitch White <white.mitchell.f@gmail.com>
2025-07-30 20:21:17 -0500

    Initial commit with local Xcode project files

---
+++ osmo/osmoApp.swift
@@ -0,0 +1,32 @@
+//
+//  osmoApp.swift
+//  osmo
+//
+//  Created by Mitchell White on 7/30/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct osmoApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ osmoTests/osmoTests.swift
@@ -0,0 +1,17 @@
+//
+//  osmoTests.swift
+//  osmoTests
+//
+//  Created by Mitchell White on 7/30/25.
+//
+
+import Testing
+@testable import osmo
+
+struct osmoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
